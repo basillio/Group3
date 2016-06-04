d24ce32eb48a228ea1e6b054effaf91e8bba39f3
Vasiliy <vasiliy.berezovsky@gmail.com>
2016-06-04 22:11:33 +0300

    module 3 changed module 4 changed

--- src/Module4/figureSquare/Circle.java
+++ src/Module4/figureSquare/Circle.java
@@ -2,9 +2,6 @@ package Module4.figureSquare;
 
 
 class Circle extends Figure {
-    double radius;
-
-   private double square = Math.PI * radius * radius;
 
     public double getCircleSquare(double radius){
         double square = Math.PI * radius * radius;
--- src/Module4/figureSquare/Figure.java
+++ src/Module4/figureSquare/Figure.java
@@ -3,9 +3,6 @@ package Module4.figureSquare;
 abstract class Figure {
 
 
-   // CountSquare countSquare = new CountSquare();
-    //Circle circle = new Circle();
-    //Triangle triangle = new Triangle();
-    //Rectangle rectangle = new Rectangle();
+
 
 }
