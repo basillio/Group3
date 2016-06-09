b503c4a8b88466a79a9c2e580f5d8d8583ad1362
vberezovskii <vasiliy.berezovsky@gmail.com>
2016-06-09 16:44:01 +0300

    New ierarchy

--- src/Module5/MinMax/MinMax.java
+++ src/Module5/MinMax/MinMax.java
@@ -1,7 +1,36 @@
 package Module5.MinMax;
 
-/**
- * Created by vberezovskii on 08.06.2016.
- */
+import java.util.Random;
+
 public class MinMax {
+    public static void main(String[] args) {
+
+
+        int[] intArray = new int[10];
+        Random rand  = new Random();
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = rand.nextInt(50);
+            System.out.print(intArray [i] + " ");
+        }
+        int max = intArray[0];
+        int min = intArray[0];
+        for (int i = 0; i<intArray.length; i++)
+        {
+            if (max < intArray[i])
+                max = intArray[i];
+            else {
+                if (min > intArray[i])
+                    min = intArray[i];
+            }
+            }
+        System.out.println("Maximum = " + max);
+
+
+        System.out.println("Minuimum = " + min);
+        }
+
+
+
+
+
 }
