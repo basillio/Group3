8935584d49ef1f52e79cb646c4a38d175fac563f
vberezovskii <vasiliy.berezovsky@gmail.com>
2016-06-20 11:05:14 +0300

    New ierarchy

---
+++ src/Module5/SelectionSort/SelectionSort.java
@@ -0,0 +1,43 @@
+package Module5.SelectionSort;
+
+import java.util.Random;
+
+/**
+ * Created by vberezovskii on 20.06.2016.
+ */
+public class SelectionSort {
+    public static void main(String[] args){
+        int[] arr = new int[10];
+        Random rand = new Random();
+        System.out.println("Original array: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt(50);
+
+            System.out.println(arr [i] + " ");
+        }
+        System.out.println("Sorted array: ");
+            for (int i = 0; i < arr.length; i++) {
+
+            int min = arr[i];
+            int min_i = i;
+
+            for (int j = i+1; j < arr.length; j++) {
+
+                if (arr[j] < min) {
+                    min = arr[j];
+                    min_i = j;
+                }
+            }
+
+            if (i != min_i) {
+                int tmp = arr[i];
+                arr[i] = arr[min_i];
+                arr[min_i] = tmp;
+            }
+
+                System.out.println(arr [i] + " ");
+
+        }
+
+    }
+}
